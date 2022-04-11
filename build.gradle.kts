2d3c85ebf08d744f94aca6df6d2d671c7a54bdc3
vmilojevic <40184129+vmilojevic@users.noreply.github.com>
2022-04-12 00:52:38 +0200

    Avro generation support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.4"
+    id("org.springframework.boot") version "2.6.6"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
+    id("com.github.davidmc24.gradle.plugin.avro") version "1.1.0"
 }
 
 group = "com.example"
@@ -13,13 +14,26 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
+    maven(
+        url = "https://packages.confluent.io/maven/"
+    )
 }
+
+extra["apacheAvroVersion"] = "1.11.0"
+extra["confluentKafkaVersion"] = "7.0.1"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.apache.avro:avro:${property("apacheAvroVersion")}")
     implementation("org.springframework.kafka:spring-kafka")
+    implementation("io.confluent:kafka-avro-serializer:${property("confluentKafkaVersion")}")
+    implementation("io.confluent:kafka-schema-registry-client:${property("confluentKafkaVersion")}")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
 }
@@ -33,4 +47,9 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.generateAvroJava {
+    source = fileTree("src/main/resources/avro")
+    setCreateSetters("false")
 }
